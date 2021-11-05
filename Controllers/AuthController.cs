48caa779dab0e76c2486d9b93abe0389cbf54850
felipe <baz@xds.com.br>
2021-11-05 16:01:29 -0300

    Add JWT Bearer token Auth for a login with roles, username and password

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using teste.Models;
+using teste.Repository;
+using teste.Services;
+using teste.ViewModel;
+
+namespace teste.Controllers
+{
+    [ApiController]
+    [Authorize]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost]
+        [Route("login")]
+        [AllowAnonymous]
+        public ActionResult<dynamic> Authenticate([FromBody] LoginView model)
+        {
+            // Recupera o usuário
+            var user = UserRepository.Get(model.Username, model.Password);
+
+            // Verifica se o usuário existe
+            if (user == null)
+                return NotFound(new { message = "Usuário ou senha inválidos" });
+
+            // Gera o Token
+            var token = TokenService.GenerateToken(user);
+
+            // Oculta a senha
+            user.Password = "";
+
+            // Retorna os dados
+            return new
+            {
+                user = user,
+                token = token
+            };
+        }
+
+        [HttpGet]
+        [Route("anonymous")]
+        [AllowAnonymous]
+        public string Anonymous() => "Anônimo";
+
+        [HttpGet]
+        [Route("authenticated")]
+        [Authorize]
+        public string Authenticated() => String.Format("Autenticado - {0}", User.Identity.Name);
+
+        [HttpGet]
+        [Route("employee")]
+        [Authorize(Roles = "employee,manager")]
+        public string Employee() => "Funcionário";
+
+        [HttpGet]
+        [Route("manager")]
+        [Authorize(Roles = "manager")]
+        public string Manager() => "Gerente";
+    }
+}
---
+++ Settings.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace teste
+{
+    public class Settings
+    {
+        public static string Secret = "22e46348f7ce8517f09bf39c534b3aabaa83fb5ae3a729d009612cfd9a0c33dd7650faff84f5ba1da41112e8b1a7725cbb35c8b3a3fec5020b641df68ee86a19";
+    }
+}
